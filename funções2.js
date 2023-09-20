936d977c9a068d377cba4a786b744a0c5d401906
Eduardo Alvarez <opticoedu@gmail.com>
2023-09-20 11:54:59 -0300

    added 2 files of functions exercises

---
+++ funções2.js
@@ -0,0 +1,22 @@
+
+
+/* 1 - Crie uma função anônima para gerar uma mensagem de convite de casamento, que
+deve receber nomeConvidado, data, horário, local e nomeNoivos e a partir disso
+monte uma mensagem convidando a pessoa para o casamento. Realize pelo menos 3
+chamadas para ela e valide se o retorno foi o esperado.*/
+
+
+
+
+
+/* 2 - Crie uma calculadora de média que solicite ao usuário um conjunto de números e
+calcule a média deles. Use uma função autoinvocada para encapsular o código da
+calculadora.*/
+
+
+
+
+
+/* 3 - Crie um Array numérico com 10 elementos e a partir dele utilize os métodos
+mostrados para verificar se todos são ímpares. Além disse crie outro Array a partir
+dele onde os novos elementos devem ser os antigos ao quadrado.*/
---
+++ funções3.js
@@ -0,0 +1,90 @@
+//Atividade (Arrow Functions)
+
+/* 1 - Uma construtora precisa de um sistema que realiza o cálculo da aréa de diversos
+terrenos retangulares por meio de uma função, você recebeu o relatório com as
+medidas dos lados dos terrenos e precisa repassar os valores das áreas. Sendo assim,
+crie a função para realizar os cálculos dos seguintes terrenos (crie uma função e faça 4
+chamadas passando diferentes valores de parâmetros):
+- Terreno padrão: largura: 50m / profundidade: 40m
+- Terreno sobrado: largura: 30m / profundidade: 40m
+- Terreno mansão: largura: 100m / profundidade: 100m
+- Terreno comercial: largura: 40m / profundidade: 40m*/
+
+
+
+/* 2 - A partir da função de área retangular criada anteriormente, crie uma função que calcula
+a área de um triangulo, a qual deve receber um valor de base e altura como argumento,
+calcular a área pela função área e divide o resultado por 2 para ter o retorno correto.*/
+
+
+
+/* 4 - Crie uma função que determine quantos números primos existem dentro de um
+intervalo que o usuário informar. Utilize uma abordagem de repetição com for ou while.
+Utilizando funções, fazer um sistema que receba um número e retorne se ele é par ou
+ímpar.
+
+
+
+/* 5 - Criar uma calculadora utilizando funções para cada operação. O usuário deve digitar o
+primeiro número, o segundo número e em seguida a operação que deseja realizar (1 -
+Soma 2 - Subtração 3 - Multiplicação 4 - Divisão). O resultado deve ser mostrado na tela.
+Digite o primeiro número:
+Digite o segundo número:
+Qual operação deseja realizar? 1 – Soma 2 – Subtração 3 – Multiplicação 4 - Divisão*/
+
+/* 6 - Crie 2 funções: a primeira recebe 3 números como argumento, realize a soma desses
+3 números e chama a segunda função, que por sua vez recebe como argumento
+uma soma e uma quantidade e retorna a média. Armazene o resultado da média em
+uma variável e exiba por um alert.
+Faça um programa que recebe três números do usuário, e identifica o maior através
+de uma função e o menor número através de outra função e depois os exibe por um
+alert.
+
+
+
+/* 7 - Crie uma função que recebe um número inteiro e retorna todos os primos, de 1 até
+ele.*/
+
+
+
+/* 8 - Faça um programa que recebe três números do usuário, e identifica o maior através
+de uma função e o menor número através de outra função e depois os exibe por um
+alert.*/
+
+
+
+//Atividade (Arrow Functions + métodos de Array)
+
+/* 1 Crie um programa que recebe 10 números de inscrição para um campeonato, a partir
+desses números, crie um array para os jogadores do time de números pares e um outro
+para os jogadores do time de números ímpares e exiba na tela se os times estão com a
+mesma quantidade de jogadores.*/
+
+
+
+/* 2 - Crie um laço de repetição para solicitar 5 idades e armazená-las em um array, use o
+método every para verificar se todos os usuários são maiores de idade.*/
+
+
+/*DESAFIO REDUCE 1: Crie um laço de repetição para solicitar 5 números, armazene-os em
+um array e use o método reduce para encontrar e mostrar o maior valor no array.*/
+
+
+
+
+/* 3 Crie um array de preços e use o método filter para criar um novo array que contenha
+apenas os preços inferioriores a R$ 50.*/
+
+
+/* Fazer um programa no qual o usuário deve montar uma salada de frutas com uma
+cereja no final. Deve ser perguntado em sequência “Qual fruta adicionar?”. Quando
+for adicionado a cereja, finalizar com a frase “Sua salada de frutas está pronta!”.
+Mostrar a lista das frutas. (DESAFIO: Mostrar também a quantidade de cada fruta)*/
+
+
+
+/* DESAFIO REDUCE 2: Crie uma calculadora de médias solicitando as notas por meio
+de um laço de repetição while, onde o usuário tenha a opção de digitar uma nova
+nota ou verificar resultado digitando “-1”. Armazene as notas em um Array e utilize
+o método reduce para obter a soma delas, após ter a soma calcule a média dividindo
+a soma pelo tamanho do array (array.length).*/
